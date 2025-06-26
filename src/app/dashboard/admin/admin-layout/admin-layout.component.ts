7e6f4dd74e88ec2e871368b313b9e6281e072843
luffybrad <bradleykahoron399@gmail.com>
2025-06-26 21:19:01 +0300

    Dashboard setup

--- src/app/dashboard/admin/admin-layout/admin-layout.component.ts
+++ src/app/dashboard/admin/admin-layout/admin-layout.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../../../auth/auth.service';
 
 @Component({
   selector: 'app-admin-layout',
@@ -9,4 +10,16 @@ import { RouterModule } from '@angular/router';
   templateUrl: './admin-layout.component.html',
   styleUrls: ['./admin-layout.component.css'],
 })
-export class AdminLayoutComponent {}
+export class AdminLayoutComponent {
+  sidebarOpen = false;
+
+  constructor(private authService: AuthService) {}
+
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
+
+  logout() {
+    this.authService.logout(); // Will redirect and clear session
+  }
+}
