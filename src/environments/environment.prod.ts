6c97854151a3b892032fb53d42aac05bd27cd0a7
luffybrad <bradleykahoron399@gmail.com>
2025-07-07 11:14:44 +0300

    vercel deploy config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://98ee-197-248-103-242.ngrok-free.app/api',
+  apiUrl: 'https://kbhs-server.onrender.com/api',
 };
